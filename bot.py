a28221b1f83b5bbae0929dadcaed6c32a2efecd8
skillor <jack-dark1@gmx.de>
2021-05-14 22:03:48 +0200

    added old new role

--- bot.py
+++ bot.py
@@ -163,6 +163,8 @@ class DiscordBot:
         data_obj = {'member': member,
                     'old_lvl': old_lvl,
                     'new_lvl': new_lvl,
+                    'old_role': old_role,
+                    'new_role': new_role,
                     'old_color': imgtools.rgb_to_bgr(new_role.color.to_rgb()),
                     'new_color': imgtools.rgb_to_bgr(old_role.color.to_rgb()),
                     'name': name}
