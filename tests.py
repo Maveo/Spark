95088354d1e9e1e9ed475baaf38d861ec188ae95
skillor <jack-dark1@gmx.de>
2021-05-15 16:58:26 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -64,8 +64,9 @@ class MemberDummy:
 
 
 class MessageDummy:
-    def __init__(self, author=MemberDummy(0)):
+    def __init__(self, author=MemberDummy(0), guild=GuildDummy(0)):
         self.author = author
+        self.guild = guild
 
 
 def main():
@@ -78,9 +79,8 @@ def main():
     import os
     import asyncio
     import aiohttp
-    from helpers import tools
 
-    SHOW_IMAGES = True
+    SHOW_IMAGES = False
 
     def show_image(img):
         if SHOW_IMAGES:
@@ -97,18 +97,18 @@ def main():
             self.lvlsys_db = lvlsys_db
 
         # test init
-        async def test_b1(self):
+        async def test_1_init(self):
             return self.user_db.all() == []
 
-        # test join server
-        async def test_b1_1(self):
+        # test user joined server
+        async def test_2_user_joined(self):
             m = MemberDummy()
             await self.bot.events.on_member_join(m)
             user = await self.bot.get_user(m)
             return user['uid'] == 0 and user['lvl'] == 1 and user['xp'] == 0 and len(m.messages) == 1
 
-        # test leave server
-        async def test_b1_2(self):
+        # test user left server
+        async def test_3_user_left(self):
             m = MemberDummy()
             await self.bot.events.on_member_join(m)
             await self.bot.events.on_member_remove(m)
@@ -118,40 +118,69 @@ def main():
             except KeyError:
                 return True
 
-        # test join vc
-        async def test_b2(self):
-            m = MemberDummy()
-            await self.bot.events.on_voice_state_update(MemberDummy(0), ChannelDummy(), ChannelDummy(1))
-            user = await self.bot.get_user(m)
-            return user['uid'] == 0 and user['lvl'] == 1 and user['xp'] == 0
-
         # test write message
-        async def test_b2_1(self):
-            m = MemberDummy()
-            await self.bot.events.on_message(MessageDummy(author=m))
+        async def test_4_user_writes_message(self):
+            g = GuildDummy()
+            m = MemberDummy(guild=g)
+            msg = MessageDummy(author=m)
+            await self.bot.events.on_message(msg)
             user = await self.bot.get_user(m)
             return user['uid'] == 0 and user['lvl'] == 1 and user['xp'] == 2.5
 
-        # test leveling 1
-        async def test_b3(self):
-            m = MemberDummy()
-            await self.bot.member_joined_vc(m, 0)
-            await self.bot.member_left_vc(m, 60 * 60 * 1)
-            user = await self.bot.get_user(m)
-            return user['uid'] == 0 and user['lvl'] == 1 and user['xp'] == 60
-
-        # test bot not leveling
-        async def test_b3_1(self):
-            m = MemberDummy(bot=True)
-            await self.bot.events.on_message(MessageDummy(author=m))
+        # test bot writes message
+        async def test_5_bot_writes_message(self):
+            g = GuildDummy()
+            m = MemberDummy(guild=g, bot=True)
+            msg = MessageDummy(author=m)
+            await self.bot.events.on_message(msg)
             try:
                 await self.bot.get_user(m)
                 return False
             except TypeError:
                 return True
 
-        # test leveling 2
-        async def test_b3_2(self):
+        # test user join vc
+        async def test_6_user_join_voice_channel(self):
+            g = GuildDummy()
+            m = MemberDummy(guild=g)
+            await self.bot.events.on_voice_state_update(m, ChannelDummy(), ChannelDummy(1))
+            user = await self.bot.get_user(m)
+            return user['uid'] == 0 and user['lvl'] == 1 and user['xp'] == 0
+
+        # test bot join vc
+        async def test_7_bot_join_voice_channel(self):
+            g = GuildDummy()
+            m = MemberDummy(guild=g, bot=True)
+            await self.bot.events.on_voice_state_update(m, ChannelDummy(), ChannelDummy(1))
+            try:
+                await self.bot.get_user(m)
+                return False
+            except TypeError:
+                return True
+
+        # test user leave vc
+        async def test_8_user_leave_voice_channel(self):
+            g = GuildDummy()
+            m = MemberDummy(guild=g)
+            await self.bot.member_joined_vc(m, 0)
+            await self.bot.member_left_vc(m, 60 * 60 * 1)
+            user = await self.bot.get_user(m)
+            return user['uid'] == 0 and user['lvl'] == 1 and user['xp'] == 60
+
+        # test bot leave vc
+        async def test_9_bot_leave_voice_channel(self):
+            g = GuildDummy()
+            m = MemberDummy(guild=g, bot=True)
+            await self.bot.events.on_voice_state_update(m, ChannelDummy(), ChannelDummy(1))
+            await self.bot.events.on_voice_state_update(m, ChannelDummy(1), ChannelDummy())
+            try:
+                await self.bot.get_user(m)
+                return False
+            except TypeError:
+                return True
+
+        # test user leveling
+        async def test_10_leveling(self):
             m = MemberDummy()
             await self.bot.member_joined_vc(m, 0)
             await self.bot.member_left_vc(m, 60 * 60 * 5)
@@ -159,19 +188,19 @@ def main():
             return user['uid'] == 0 and user['lvl'] == 3 and user['xp'] == 90
 
         # test set lvlsys point
-        async def test_b5(self):
+        async def test_11_set_lvlsys_point(self):
             await self.bot.lvlsys_set(0, 0, 5)
             return self.lvlsys_db.all() == [{'gid': 0, 'lvlsys': {'5': 0}}]
 
         # test remove lvlsys point
-        async def test_b6(self):
+        async def test_12_remove_lvlsys_point(self):
             await self.bot.lvlsys_set(0, 0, 5)
             await self.bot.lvlsys_set(0, 0, 6)
             await self.bot.lvlsys_remove(0, 5)
             return self.lvlsys_db.all() == [{'gid': 0, 'lvlsys': {'6': 0}}]
 
-        # test role
-        async def test_b7(self):
+        # test giving role
+        async def test_13_giving_role(self):
             await self.bot.lvlsys_set(0, 0, 0)
             await self.bot.lvlsys_set(0, 1, 2)
             await self.bot.lvlsys_set(0, 2, 5)
@@ -186,8 +215,8 @@ def main():
             await self.bot.member_left_vc(m, 0)
             return m.roles == {1: True}
 
-        # test leaderboard
-        async def test_b8(self):
+        # test ranking
+        async def test_14_ranking(self):
             g = GuildDummy()
             m0 = MemberDummy(0, guild=g)
             m1 = MemberDummy(1, guild=g)
@@ -199,53 +228,41 @@ def main():
             l = await self.bot.get_ranking(g)
             return l[0]['uid'] == 1 and l[1]['uid'] == 0
 
-        # test leaderboard rank
-        async def test_b9(self):
-            g = GuildDummy()
-            m0 = MemberDummy(guild=g)
-            m1 = MemberDummy(guild=g)
-            await self.bot.member_joined_vc(m0, 0)
-            await self.bot.member_joined_vc(m1, 0)
-            await self.bot.member_left_vc(m0, 60 * 60 * 1)
-            await self.bot.member_left_vc(m1, 60 * 60 * 5)
-            rank = await self.bot.get_ranking_rank(m0)
-            return rank == 2
-
         # test profile image creation
-        async def test_b_1(self):
+        async def test_15_profile_image(self):
             m = MemberDummy()
             await self.bot.member_joined_vc(m, 0)
             await self.bot.member_set_lvl_xp(m, 5, 50)
             image_buffer = (await self.bot.member_create_profile_image(m)).fp.getbuffer()
             image = cv2.imdecode(np.frombuffer(image_buffer, np.uint8), -1)
 
-            # show_image(image)
+            show_image(image)
             return True
 
-        # test levelup image creation
-        async def test_b_2(self):
+        # test level up image creation
+        async def test_16_level_up_image(self):
             m = MemberDummy()
             image_buffer = (await self.bot.member_create_lvl_image(m, 1, 2)).fp.getbuffer()
             image = cv2.imdecode(np.frombuffer(image_buffer, np.uint8), -1)
 
-            # show_image(image)
+            show_image(image)
             return True
 
-        # test rankup image creation
-        async def test_b_3(self):
+        # test rank up image creation
+        async def test_17_rank_up_image(self):
             m = MemberDummy()
             r1 = RoleDummy()
             r2 = RoleDummy()
             image_buffer = (await self.bot.member_create_rank_up_image(m, 1, 2, r1, r2)).fp.getbuffer()
             image = cv2.imdecode(np.frombuffer(image_buffer, np.uint8), -1)
 
-            # show_image(image)
+            show_image(image)
             return True
 
         # test leaderboard image creation
-        async def test_b_4(self):
+        async def test_18_leaderboard_image(self):
             g = GuildDummy()
-            m = [MemberDummy(x, guild=g) for x in range(30)]
+            m = [MemberDummy(x, guild=g) for x in range(10)]
             [await self.bot.check_member(x) for x in m]
 
             image_buffer = (await self.bot.create_leaderboard_image(m[0])).fp.getbuffer()
@@ -254,7 +271,7 @@ def main():
             show_image(image)
             return True
 
-    async def run_test(test_name):
+    async def run_test(method, test_number, test_name):
         user_db = TinyDB(storage=MemoryStorage)
         lvlsys_db = TinyDB(storage=MemoryStorage)
 
@@ -263,22 +280,29 @@ def main():
 
         t = Tests(b, user_db, lvlsys_db)
 
-        print("TEST {}: ".format(test_name[5:]), end='')
+        print("TEST {:03d}: ".format(test_number), end='')
 
         start = time.time()
-        if await getattr(t, test_name)():
-            print("SUCCESS! elapsed {}ms".format(round((time.time() - start) * 1000, 1)))
+        if await getattr(t, method)():
+            print("SUCCESS! elapsed {}ms | {}".format(round((time.time() - start) * 1000, 1), test_name))
         else:
-            print("FAILED! elapsed {}ms".format(round((time.time() - start) * 1000, 1)))
+            print("FAILED! elapsed {}ms | {}".format(round((time.time() - start) * 1000, 1), test_name))
 
         await b.image_creator.session.close()
 
     if os.name == 'nt':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
+    tests = []
     for method_name in sorted(dir(Tests)):
         if callable(getattr(Tests, method_name)) and method_name[:5] == 'test_':
-            asyncio.run(run_test(method_name))
+            splitted_name = method_name[5:].split('_')
+            tests.append({'method_name': method_name,
+                          'test_number': int(splitted_name[0]),
+                          'test_name': ' '.join(splitted_name[1:])})
+
+    for test in sorted(tests, key=lambda x: x['test_number']):
+        asyncio.run(run_test(test['method_name'], test['test_number'], test['test_name']))
 
 
 if __name__ == '__main__':
