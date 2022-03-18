b047d0c12c4b83863632e3accbbcddd33fd6df35
skillor <skillor@gmx.net>
2022-03-18 18:15:52 +0100

    feat: add example module

---
+++ modules/_example/settings.py
@@ -0,0 +1,9 @@
+from helpers.settings_manager import Setting
+
+SETTINGS = {
+    'EXAMPLE_SETTING': Setting(
+        value=False,
+        description='An example setting',
+        itype='bool'
+    ),
+}
