101b8d463bfd3892f96244228705b1ae8d6a1003
skillor <skillor@gmx.net>
2022-03-23 21:25:25 +0100

    fix: remove old code

--- frontend/src/views/InventorySystem.vue
+++ frontend/src/views/InventorySystem.vue
@@ -167,16 +167,6 @@
                                     Infinite use
                                 </button>
                             </div>
-                            <!-- <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-                                <label class="btn btn-primary" for="btnradio1">Radio 1</label>
-
-                                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-                                <label class="btn btn-primary" for="btnradio2">Radio 2</label>
-
-                                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-                                <label class="btn btn-primary" for="btnradio3">Radio 3</label>
-                            </div> -->
                         </div>
                         <div class="mb-2">   
                             <div class="input-group input-group-sm">
