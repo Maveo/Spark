407fd4bd11a2310e0f97ce7c3a9478601e8cb399
dreiekk <dreiekk@users.noreply.github.com>
2022-04-18 19:46:48 +0200

    Removed EXAMPLE_SETTING from store module

--- modules/store/settings.py
+++ modules/store/settings.py
@@ -1,9 +1,3 @@
 from helpers.settings_manager import Setting
 
-SETTINGS = {
-    'EXAMPLE_SETTING': Setting(
-        value=False,
-        description='An example setting',
-        itype='bool'
-    ),
-}
+SETTINGS = {}
