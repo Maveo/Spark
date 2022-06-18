ca699852f57ea27c7ed59d81c7ec0ab7d14b9629
skillor <skillor@gmx.net>
2022-06-18 18:25:09 +0200

    fix: general module

--- modules/general/web.py
+++ modules/general/web.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 import discord
 from discord import PCMAudio, ActivityType, Status, Activity
-from flask import jsonify, request, send_file
+from fastapi import Body, UploadFile, File, Form
 from imagestack_svg.helpers import is_emoji, from_char
 
 from helpers.exceptions import WrongInputException, MethodNotAvailableException
@@ -13,7 +13,9 @@ from helpers.module_pages import has_permissions
 from helpers.tools import search_text_channel, search_voice_channel
 from webserver import Page
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Union
+
+from webserver.responses import BytesFileResponse
 
 AUDIO_IMPORTED = False
 try:
@@ -30,99 +32,99 @@ if TYPE_CHECKING:
 async def get_profile(module: 'GeneralModule',
                       guild: discord.Guild,
                       member: discord.Member):
-    return jsonify(await module.bot.module_manager.create_extended_profile(member)), 200
+    return await module.bot.module_manager.create_extended_profile(member)
 
 
 @has_permissions(administrator=True)
 async def get_settings(module: 'GeneralModule',
                        guild: discord.Guild,
                        member: discord.Member):
-    return jsonify({
+    return {
         'categories': module.bot.module_manager.settings.categories(guild.id),
         'settings': module.bot.module_manager.settings.all_as_dict(guild.id)
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def reset_setting(module: 'GeneralModule',
                         guild: discord.Guild,
-                        member: discord.Member):
-    json = request.get_json()
-    if json is None or 'key' not in json:
-        raise WrongInputException('setting key or value not provided')
-    if json['key'] not in module.bot.module_manager.settings.keys(guild.id):
-        raise WrongInputException('setting key not correct')
-    module.bot.module_manager.settings.remove(guild.id, json['key'])
+                        member: discord.Member,
+                        key: str = Body(embed=True),
+                        ):
+    if key not in module.bot.module_manager.settings.keys(guild.id):
+        raise WrongInputException(detail='setting key not correct')
+    module.bot.module_manager.settings.remove(guild.id, key)
 
-    return jsonify({
+    return {
         'msg': 'success',
-        'value': module.bot.module_manager.settings.get(guild.id, json['key'])
-    }), 200
+        'value': module.bot.module_manager.settings.get(guild.id, key)
+    }
 
 
 @has_permissions(administrator=True)
 async def set_setting(module: 'GeneralModule',
                       guild: discord.Guild,
-                      member: discord.Member):
-    json = request.get_json()
-    if json is None or 'key' not in json or 'value' not in json:
-        raise WrongInputException('setting key or value not provided')
-    if json['key'] not in module.bot.module_manager.settings.keys(guild.id):
-        raise WrongInputException('setting key not correct')
-    if not module.bot.module_manager.settings.set(guild.id, json['key'], json['value']):
-        raise WrongInputException('setting value not correct')
+                      member: discord.Member,
+                      key: str = Body(embed=True),
+                      value: Any = Body(embed=True),
+                      ):
+    if key not in module.bot.module_manager.settings.keys(guild.id):
+        raise WrongInputException(detail='setting key not correct')
+    if not module.bot.module_manager.settings.set(guild.id, key, value):
+        raise WrongInputException(detail='setting value not correct')
 
-    return jsonify({
+    return {
         'msg': 'success',
-        'value': module.bot.module_manager.settings.get(guild.id, json['key'])
-    }), 200
+        'value': module.bot.module_manager.settings.get(guild.id, key)
+    }
 
 
 @has_permissions(administrator=True)
 async def get_modules(module: 'GeneralModule',
                       guild: discord.Guild,
                       member: discord.Member):
-    return jsonify({
+    return {
         'modules': list(module.bot.module_manager.values())
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
-async def set_module(module: 'GeneralModule',
+async def set_module(x_module: 'GeneralModule',
                      guild: discord.Guild,
-                     member: discord.Member):
-    json = request.get_json()
-    if json is None or 'module' not in json or 'activate' not in json:
-        raise WrongInputException('module or activate not provided')
-    if json['activate']:
-        await module.bot.module_manager.activate_module(guild.id, json['module'], True)
+                     member: discord.Member,
+                     module: str = Body(embed=True),
+                     activate: bool = Body(embed=True),
+                     ):
+    if activate:
+        await x_module.bot.module_manager.activate_module(guild.id, module, True)
     else:
-        await module.bot.module_manager.deactivate_module(guild.id, json['module'], True)
-    return jsonify({'msg': 'success'}), 200
+        await x_module.bot.module_manager.deactivate_module(guild.id, module, True)
+    return {'msg': 'success'}
 
 
 async def welcome_image(module: 'GeneralModule',
                         guild: discord.Guild,
-                        member: discord.Member):
-    json = request.get_json()
-    if json is None or 'preview' not in json:
+                        member: discord.Member,
+                        preview: Union[str, None] = Body(default=None, embed=True),
+                        ):
+    if preview is None:
         img = await module.member_create_welcome_image(member)
-        return send_file(
+        return BytesFileResponse(
             img.fp,
-            attachment_filename=img.filename,
-            mimetype='image/png'
+            filename=img.filename,
+            media_type='image/png',
         )
 
     try:
-        preview = module.bot.module_manager.settings.preview(guild.id, 'WELCOME_IMAGE', json['preview'])
+        preview = module.bot.module_manager.settings.preview(guild.id, 'WELCOME_IMAGE', preview)
     except:
-        raise WrongInputException('setting preview not correct')
+        raise WrongInputException(detail='setting preview not correct')
 
     img = await module.member_create_welcome_image_by_template(member, preview)
-    return send_file(
+    return BytesFileResponse(
         img.fp,
-        attachment_filename=img.filename,
-        mimetype='image/png'
+        filename=img.filename,
+        media_type='image/png',
     )
 
 
@@ -130,157 +132,133 @@ async def welcome_image(module: 'GeneralModule',
 async def get_invite_links(module: 'GeneralModule',
                            guild: discord.Guild,
                            member: discord.Member):
-    return jsonify({
+    return {
         'invite_links': asyncio.run_coroutine_threadsafe(guild.invites(), module.bot.bot.loop).result()
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def invite_link(module: 'GeneralModule',
                       guild: discord.Guild,
-                      member: discord.Member):
-    json = request.get_json()
-    if json is None:
-        json = {}
-
+                      member: discord.Member,
+                      json_body: dict = Body(default={})):
     channel = None
-    if 'search_channel' in json:
-        channel = search_text_channel(guild, json['search_channel'])
-        del json['search_channel']
+    if 'search_channel' in json_body:
+        channel = search_text_channel(guild, json_body['search_channel'])
+        del json_body['search_channel']
     if channel is None:
         channel = guild.system_channel
-    return jsonify({
+    return {
         'invite_link': asyncio.run_coroutine_threadsafe(
-            channel.create_invite(**json), module.bot.bot.loop).result()
-    }), 200
+            channel.create_invite(**json_body), module.bot.bot.loop).result()
+    }
 
 
 @has_permissions(administrator=True)
 async def text_channels(module: 'GeneralModule',
                         guild: discord.Guild,
                         member: discord.Member):
-    return jsonify({
+    return {
         'text_channels': guild.text_channels
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def voice_channels(module: 'GeneralModule',
                          guild: discord.Guild,
                          member: discord.Member):
-    return jsonify({
+    return {
         'voice_channels': guild.voice_channels
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def send_msg_channel(module: 'GeneralModule',
                            guild: discord.Guild,
-                           member: discord.Member):
-    json = request.get_json()
-    if json is None or 'channel_id' not in json or 'message' not in json:
-        raise WrongInputException('channel_id or message not provided')
-
-    channel = search_text_channel(guild, json['channel_id'])
+                           member: discord.Member,
+                           channel_id: str = Body(embed=True),
+                           message: str = Body(embed=True),
+                           ):
+    channel = search_text_channel(guild, str(channel_id))
     if channel is None:
-        raise WrongInputException('channel not found')
+        raise WrongInputException(detail='channel not found')
 
-    asyncio.run_coroutine_threadsafe(channel.send(str(json['message'])), module.bot.bot.loop).result()
+    asyncio.run_coroutine_threadsafe(channel.send(str(message)), module.bot.bot.loop).result()
 
-    return jsonify({
+    return {
         'msg': 'success',
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def get_messages(module: 'GeneralModule',
                        guild: discord.Guild,
-                       member: discord.Member):
-    params = request.args
-    if params is None or 'channel_id' not in params:
-        raise WrongInputException('channel_id not provided')
-
-    channel = search_text_channel(guild, params['channel_id'])
+                       member: discord.Member,
+                       channel_id: str,
+                       limit: int = 100,
+                       ):
+    channel = search_text_channel(guild, channel_id)
     if channel is None:
-        raise WrongInputException('channel not found')
+        raise WrongInputException(detail='channel not found')
 
-    limit = 100
-    if 'limit' in params and str(params['limit']).isnumeric():
-        limit = int(params['limit'])
-
-    return jsonify({
+    return {
         'messages': asyncio.run_coroutine_threadsafe(
             channel.history(limit=limit).flatten(), module.bot.bot.loop).result()
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def set_nickname(module: 'GeneralModule',
                        guild: discord.Guild,
-                       member: discord.Member):
-    json = request.get_json()
-    if json is None or 'nickname' not in json:
-        raise WrongInputException('nickname not provided')
+                       member: discord.Member,
+                       nickname: str = Body(embed=True),
+                       ):
+    asyncio.run_coroutine_threadsafe(guild.me.edit(nick=nickname), module.bot.bot.loop).result()
 
-    asyncio.run_coroutine_threadsafe(guild.me.edit(nick=json['nickname']), module.bot.bot.loop).result()
-
-    return jsonify({
+    return {
         'msg': 'success',
-    }), 200
+    }
 
 
 @has_permissions(administrator=True)
 async def send_voice_audio(module: 'GeneralModule',
                            guild: discord.Guild,
-                           member: discord.Member):
-    if 'audio_file' not in request.files or 'voice_channel' not in request.form:
-        raise WrongInputException('audio_file or voice_channel not provided')
-
+                           member: discord.Member,
+                           audio_file: UploadFile = File(),
+                           voice_channel: str = Form(),
+                           ):
     if not AUDIO_IMPORTED:
-        raise WrongInputException('audio not supported')
+        raise WrongInputException(detail='audio not supported')
 
     stream = BytesIO()
-    sound = AudioSegment.from_file(request.files['audio_file'])
+    sound = AudioSegment.from_file(audio_file.file)
     sound.export(stream, format='wav')
     await module.bot.play_audio(
         PCMAudio(stream),
-        search_voice_channel(guild, request.form['voice_channel'])
+        search_voice_channel(guild, voice_channel)
     )
 
-    # request.files['audio_file'].save('__temp.mp3')
-    # await self.dbot.play_audio(
-    #     FFmpegPCMAudio('__temp.mp3'),
-    #     await self.dbot.search_voice_channel(guild, request.form['voice_channel'])
-    # )
-
-    return jsonify({
+    return {
         'msg': 'success',
-    }), 200
+    }
 
 
 @has_permissions(super_admin=True)
 async def set_presence(module: 'GeneralModule',
                        guild: discord.Guild,
-                       member: discord.Member):
-    json = request.get_json()
-    if json is None:
-        json = {}
-
-    if 'activity_name' not in json:
-        json['activity_name'] = None
-    if 'activity_type' not in json or json['activity_type'] is None:
-        json['activity_type'] = ActivityType.custom
-    if 'status_type' not in json or json['status_type'] is None:
-        json['status_type'] = Status.online
-
+                       member: discord.Member,
+                       activity_name: Union[str, None] = Body(embed=True, default=None),
+                       activity_type: Union[int, None] = Body(embed=True, default=ActivityType.custom),
+                       status_type: Union[str, None] = Body(embed=True, default=Status.online),
+                       ):
     asyncio.run_coroutine_threadsafe(
         module.bot.bot.change_presence(
-            activity=Activity(type=json['activity_type'], name=json['activity_name']), status=json['status_type']),
+            activity=Activity(type=activity_type, name=activity_name), status=status_type),
         module.bot.bot.loop).result()
 
-    return jsonify({
+    return {
         'msg': 'success',
-    }), 200
+    }
 
 
 @has_permissions(super_admin=True)
@@ -293,34 +271,35 @@ async def get_emojis(module: 'GeneralModule',
         with open(os.path.join(module.bot.image_creator.emoji_loader.emoji_path, e['path']), 'rb') as f:
             send_emojis.append({'emoji': e['emoji'], 'base64': base64.b64encode(f.read()).decode()})
 
-    return jsonify({
+    return {
         'msg': 'success',
         'emojis': send_emojis
-    }), 200
+    }
 
 
 @has_permissions(super_admin=True)
 async def change_emoji(module: 'GeneralModule',
                        guild: discord.Guild,
-                       member: discord.Member):
-    if 'emoji_file' not in request.files or 'emoji' not in request.form:
-        raise WrongInputException('emoji_file or emoji not provided')
-
-    if not is_emoji(request.form['emoji']):
-        raise WrongInputException('emoji is wrong')
+                       member: discord.Member,
+                       emoji_file: UploadFile = File(),
+                       emoji: str = Form(),
+                       ):
+    if not is_emoji(emoji):
+        raise WrongInputException(detail='emoji is wrong')
 
     if not module.bot.image_creator.emoji_loader.save_downloaded_emojis:
-        raise MethodNotAvailableException('cannot save emojis')
+        raise MethodNotAvailableException(detail='cannot save emojis')
 
-    emoji_id = from_char(request.form['emoji'])
+    emoji_id = from_char(emoji)
     module.bot.image_creator.emoji_loader.cached_images.clear()
-    request.files['emoji_file'].save(os.path.join(module.bot.image_creator.emoji_loader.emoji_path,
-                                                  emoji_id + '.png'))
+    with open(os.path.join(module.bot.image_creator.emoji_loader.emoji_path,
+                           emoji_id + '.png'), 'wb') as f:
+        f.write(emoji_file.file.read())
     module.bot.logger.info('saved new image for emoji {}'.format(emoji_id))
 
-    return jsonify({
+    return {
         'msg': 'success',
-    }), 200
+    }
 
 
 API_PAGES = [
