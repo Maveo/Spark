512b1ba5dfc7c9fe58a7dab58455f876d0826249
skillor <skillor@gmx.net>
2024-10-02 00:48:06 +0200

    Update settings.py

--- modules/active_tracker/settings.py
+++ modules/active_tracker/settings.py
@@ -16,14 +16,14 @@ activity_image = ImageStackResolveString('''<defs>
 <rect x="30" y="40" width="1140" height="180" rx="45" ry="45" fill="none" stroke="url(#color1)" stroke-width="10" />
 <text x="310" y="163" font-family="Product Sans" font-weight="bold" font-size="100" fill="rgb(255, 255, 255)">Wall of Shame</text>
 <text x="90" y="300" font-family="Product Sans" font-size="65" fill="rgb(87, 87, 87)">Name</text>
-<text x="960" y="300" font-family="Product Sans" font-size="35" fill="rgb(87, 87, 87)">Time Muted</text>
+<text x="1150" y="300" text-anchor="end" font-family="Product Sans" font-size="35" fill="rgb(87, 87, 87)">Time Muted (last {{"%0d" | format(current_duration / 3600)}} hours)</text>
 <rect x="37" y="320" width="1100" height="{{ (users | length) * 85 + 410 - 355 }}" rx="55" ry="55" fill="rgb(57, 59, 65)" />
 <rect x="950" y="320" width="200" height="{{ (users | length) * 85 + 410 - 355 }}" rx="45" ry="45" fill="url(#color2)" />
 {% for user in users %}
 	<text x="135" y="{{ (loop.index * 85) + 325 }}" text-anchor="middle" font-family="Product Sans" font-weight="bold" font-size="60" fill="rgb(255, 255, 255)">#{{ user.rank }}</text>
 	<image x="240" y="{{ (loop.index * 85) + 272 }}" width="60" height="60" xlink:href="{{ user.member.top_role.name[0] | emoji }}"/>
 	<text x="323" y="{{ (loop.index * 85) + 325 }}" font-family="Product Sans" font-size="60" fill="rgb(255, 255, 255)">{{ user.name }}</text>
-	<text x="1055" y="{{ (loop.index * 85) + 325 }}" text-anchor="middle" font-family="Product Sans" font-size="60" fill="rgb(29, 29, 29)">{{ '%d' | format(user.interval_muted_time / 60) }} m</text>
+	<text x="1055" y="{{ (loop.index * 85) + 325 }}" text-anchor="middle" font-family="Product Sans" font-size="60" fill="rgb(29, 29, 29)">{{ "%d" | format(user.interval_muted_time / 60) }} m</text>
 {% endfor %}''')
 
 SETTINGS = {
